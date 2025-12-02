4a9a8bc318c81e85e41128ccba62a96bbcd71006
zehrainoz <zehrainozz@gmail.com>
2025-12-02 17:12:59 +0300

    add controller for adding product

--- src/main/java/com/example/products/controllers/ProductsController.java
+++ src/main/java/com/example/products/controllers/ProductsController.java
@@ -1,9 +1,12 @@
 package com.example.products.controllers;
 
+import com.example.products.model.Product;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 import com.example.products.services.ProductService;
+
+import java.util.List;
 
 @Controller
 public class ProductsController {
@@ -12,11 +15,23 @@ public class ProductsController {
     public ProductsController(ProductService productService) {
         this.productService = productService;
     }
-    
-    @RequestMapping("/products")
+
+    @GetMapping("/products")
     public String viewProducts(Model model) {
         var products = productService.findAll();
         model.addAttribute("products", products);
         return "products.html";
+    }
+
+    @PostMapping("/products")
+    public String addProduct(@RequestParam String name, @RequestParam double price, Model model){
+        Product product = new Product();
+        product.setName(name);
+        product.setPrice(price);
+        productService.addProduct(product);
+
+        var products = productService.findAll();
+        model.addAttribute("products", products);
+        return "products";
     }
 }
